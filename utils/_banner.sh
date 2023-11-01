2d9f7d6b1855554ff6dbc330247afeb64b63ec24
gatoanakin <Phelipw13@gmail.com>
2023-11-01 20:26:18 -0300

    Initial commit

---
+++ utils/_banner.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+printf "${CYAN_LIGHT}";
+
+
+ printf "                                              .:^^^^^^^^^^:.        \n";
+ printf "                                            :~^^!7??JJ??7!~^^^.     \n";
+ printf "                                          .~^~?YY!?YYYYY5YYJ!^~^    \n";
+ printf "                                         :!:7YYY^        !YYYJ^^!   \n";
+ printf "                                        .7.7YYY~   JYY?  .YYYYY:~^  \n";
+ printf "                                        ^!:YYY~    ?Y!   .JYYYY7.7  \n";
+ printf "                                        ^!:YY!           .YYJ?Y7.7  \n";
+ printf "                                        .7.?!  779:7YYYYYY7!~YY:~^  \n";
+ printf "                                         ~J!  :JYJ^.^7Y5~YY:!YJ^!   \n";
+ printf "                                         ?7:!!?YYYYYYYYY??YJ!^^     \n";
+ printf "                                        ^J!?7~^~!7??JJJ??!^^        \n";
+ printf "                                        ^~:                         \n";
+
+                                                                                                                                                         
+  printf "            \033[1;33m        © CANAL Pcure
+  printf "${NC}";
+
+  printf "\n"
+}
